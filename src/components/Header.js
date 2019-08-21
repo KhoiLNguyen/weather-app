a971117858497d5f1ed1c60fd65149abe4bb0315
unknown <khoin0412@gmail.com>
2019-08-21 17:51:38 -0500

    Added comments for clarification

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react'
 
 const Header = ({ placeName }) => {
   
+  const [time, setTime] = useState(new Date()) // define time according to user's system clock
 
-  const [time, setTime] = useState(new Date())
-
+  // Wait for time to load then re-render time every one second => time is a dependency
   useEffect(() => {
     updateTime()
   }, [time])
 
+  // Function to update time every one second
   const updateTime = () => {
     setInterval(() => {
       setTime(new Date())
@@ -19,7 +20,7 @@ const Header = ({ placeName }) => {
   return (
     <div>
       <h1>Weather App</h1>
-      <h1>{time.toLocaleTimeString()}</h1>
+      <h1>{time.toLocaleTimeString()}</h1> {/*display time as hh:mm:ss - am/pm string*/}
       <h1>{placeName}</h1>
     </div>
   )
