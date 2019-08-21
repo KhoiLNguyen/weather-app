dcb2f731b859725ab94aa83f6eec1d687c3dba5b
unknown <khoin0412@gmail.com>
2019-08-21 17:40:13 -0500

    Implemented Timer Based on User PC/laptop's clock

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Header = ({ placeName }) => {
+  
+
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    updateTime()
+  }, [time])
+
+  const updateTime = () => {
+    setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+    
+  }
+
   return (
     <div>
       <h1>Weather App</h1>
-      <h1>12:00:00 - AM</h1>
+      <h1>{time.toLocaleTimeString()}</h1>
       <h1>{placeName}</h1>
     </div>
   )
