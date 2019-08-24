fc63477a8665e932120aea9042a6392f204fdb41
Khoi <khoin0412@gmail.com>
2019-08-24 17:18:05 -0500

    Updates

---
+++ precache-manifest.2be7f40cad2b67056bf46838c8fb59dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3316f2f881df0aa5d7ad176ec46f431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2de9c1fe849eea0c7af1",
+    "url": "/static/css/2.c37b5d79.chunk.css"
+  },
+  {
+    "revision": "102c2fc9f2039dd688b9",
+    "url": "/static/css/main.500cf129.chunk.css"
+  },
+  {
+    "revision": "2de9c1fe849eea0c7af1",
+    "url": "/static/js/2.59b915b1.chunk.js"
+  },
+  {
+    "revision": "102c2fc9f2039dd688b9",
+    "url": "/static/js/main.febd5fbc.chunk.js"
+  },
+  {
+    "revision": "a549e4d2d24dee02cf53",
+    "url": "/static/js/runtime~main.7a67e8d5.js"
+  }
+]);
